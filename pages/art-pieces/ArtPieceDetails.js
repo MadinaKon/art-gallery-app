6313a874f3e5e9c17769b9cb2ac8bc4e7c028e21
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-02 14:38:32 +0200

    add back to gallery button

--- pages/art-pieces/ArtPieceDetails.js
+++ pages/art-pieces/ArtPieceDetails.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ArtPieceDetails({ piece }) {
   const { imageSource, name, artist, year, genre } = piece;
   return (
     <div>
+      <Link href={`/art-pieces`}>
+        <button>Back to gallery</button>
+      </Link>
       <Image src={imageSource} width={500} height={500} alt={name} />
       <ul>
         <li>{name}</li>
