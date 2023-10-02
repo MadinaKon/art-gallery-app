db2889748d7e7a6f0cd4c2942ae470f932a9ff77
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-02 12:01:48 +0200

    add Spotlight component and function to pick a random art piece

---
+++ pages/Spotlight.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Spotlight({ image, artist }) {
+  console.log("image, artist ", image, artist);
+  return (
+    <>
+      <Image
+        src={image}
+        alt="random"
+        // width={data[indexRandomArtPiece].dimensions.width}
+        // height={data[indexRandomArtPiece].dimensions.height}
+        width="140"
+        height="140"
+      />
+      <p>{artist}</p>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,31 @@
 import useSWR, { SWRConfig } from "swr";
 import ArtPieces from "./ArtPieces";
+import Spotlight from "./Spotlight";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function HomePage() {
-  const { data, error } = useSWR(
+  const { data, isLoading, error } = useSWR(
     `https://example-apis.vercel.app/api/art`,
     fetcher
   );
 
-  console.log("DATA ", data);
+  if (isLoading) return <div>loading...</div>;
+  if (error) return <div>failed to load Error</div>;
+  function pickRandomArtPiece(array) {
+    // number between 0 and array.length
+    return Math.floor(Math.random() * array.length);
+  }
+
+  const indexRandomArtPiece = pickRandomArtPiece(data);
 
   return (
-    <SWRConfig
-      value={{
-        fetcher,
-      }}
-    >
+    <div>
+      <Spotlight
+        image={`${data[indexRandomArtPiece].imageSource}`}
+        artist={data[indexRandomArtPiece].artist}
+      />
       <ArtPieces pieces={data} />
-    </SWRConfig>
+    </div>
   );
 }
