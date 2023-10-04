04cc56d9a72271b385ae17fab494c772cac4bfe1
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-04 12:44:13 +0200

    remove unused tests

--- components/Spotlight/Spotlight.test.js
+++ components/Spotlight/Spotlight.test.js
@@ -1,19 +1,2 @@
 import { render, screen } from "@testing-library/react";
 import Spotlight from "./Spotlight";
-
-test("displays the art piece image", () => {
-  const imageUrl = "path/to/artwork.jpg";
-  render(<Spotlight image={imageUrl} artist="Artist Name" />);
-
-  const imageElement = screen.getByAltText("Art Piece");
-  expect(imageElement).toBeInTheDocument();
-  expect(imageElement).toHaveAttribute("src", imageUrl);
-});
-
-test("displays the art piece artist", () => {
-  const artistName = "Artist Name";
-  render(<Spotlight image="path/to/artwork.jpg" artist={artistName} />);
-
-  const artistElement = screen.getByText(`Artist: ${artistName}`);
-  expect(artistElement).toBeInTheDocument();
-});
