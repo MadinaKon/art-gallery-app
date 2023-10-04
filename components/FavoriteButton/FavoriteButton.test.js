5fd2c8034d8308c98792084e4bdb786561b03494
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-04 12:43:11 +0200

    remove unused tests

--- components/FavoriteButton/FavoriteButton.test.js
+++ components/FavoriteButton/FavoriteButton.test.js
@@ -2,34 +2,3 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { FavoriteButton } from "./FavoriteButton";
 import { ArtPieceDetails } from "../ArtPieceDetails/ArtPieceDetails.js";
-
-test("The favorite-button is displayed in the spotlight view", () => {
-  render(<FavoriteButton />);
-
-  const favoriteButton = screen.getAllByRole(
-    "FavoriteButton-styled__StyledFavoriteButton-sc-aee7e3bd-0 ZlMLo"
-  );
-  expect(favoriteButton).toBeInTheDocument();
-  expect(favoriteButton).toHaveClass(
-    "FavoriteButton-styled__StyledFavoriteButton-sc-aee7e3bd-0 ZlMLo"
-  );
-});
-
-const mockItems = [{ name: "Item 1" }, { name: "Item 2" }, { name: "Item 3" }];
-
-test("displays favorite button in each entry in the list view", () => {
-  render(<ArtPieceDetails items={mockItems} />);
-
-  mockItems.forEach((item) => {
-    const listItem = screen.getByText(item.name).closest("li");
-    expect(listItem).toBeInTheDocument();
-
-    const favoriteButton = within(listItem).getAllByRole(
-      "FavoriteButton-styled__StyledFavoriteButton-sc-aee7e3bd-0 ZlMLo"
-    );
-    expect(favoriteButton).toBeInTheDocument();
-    expect(favoriteButton).toHaveClass(
-      "FavoriteButton-styled__StyledFavoriteButton-sc-aee7e3bd-0 ZlMLo"
-    );
-  });
-});
