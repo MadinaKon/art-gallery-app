3db7245395ea8364acc70f942acff7e8e80a4c9e
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-10-04 12:58:41 +0200

    remove unused tests

--- components/ArtPieceDetails/ArtPieceDetails.test.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import { ArtPieceDetails } from "./ArtPieceDetails";
-import userEvent from "@testing-library/user-event";
--- components/FavoriteButton/FavoriteButton.test.js
+++
@@ -1,4 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { FavoriteButton } from "./FavoriteButton";
-import { ArtPieceDetails } from "../ArtPieceDetails/ArtPieceDetails.js";
